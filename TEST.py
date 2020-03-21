5909fe5de1694f256e4a13be3a77a18d3888b3e0
Sephidator <Sephidator@gmail.com>
2020-03-21 23:05:38 +0800

    .

--- TEST.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-
-a = torch.FloatTensor([[1,2,3],[4,5,6],[7,8,9]])
-b = torch.FloatTensor([[1,2,2],[4,2,6],[0,1,9]])
-print(a * b)
-print(torch.mul(a, b))
-print(a.exp())
--- test.py
+++
@@ -1,8 +0,0 @@
-def a():
-    return 1, 2, 3
-
-def b():
-    x, _, _  = a()
-    return x, _, _
-
-print(b())
